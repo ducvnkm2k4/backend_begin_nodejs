4c1725970cbe0ee950f8af4ba2c851f9e36f76d3
ducvnkm2k4 <duckmhd2k4@gmail.com>
2024-05-18 23:41:53 +0700

    static file

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ const port = process.env.PORT;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     res.render('sample.ejs');
 });
