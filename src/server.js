7e02ac018e49f2f4c640655cad1cf2351261aa9c
ducvnkm2k4 <duckmhd2k4@gmail.com>
2024-05-18 22:56:48 +0700

    install and use env(environment variables)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
+
 const app = express();
-const port = 8000;
+const port = process.env.PORT;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
